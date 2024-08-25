f947ebc5ede0ff8630c048c15b581b6fb563070d
ctang <11547113+crtang@users.noreply.github.com>
2024-08-24 20:50:21 -0700

    Styled header and added nav

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import lafbLogo from '../imgs/Love At First Bite logo.webp';
 
 const Header = () => {
   return (
     <header>
-      <img id="lafb-logo" src="../../imgs/" alt="Love At First Bite Bakery logo"></img>
+      <div id="header-logo-area">
+        <img id="lafb-logo" src={lafbLogo} alt="Love At First Bite Bakery logo" />
+      </div>
+
+      <nav>
+        <ul>
+          <li>HOME</li>
+          <li>MENU</li>
+          <li>ORDER</li>
+          <li>PRESS</li>
+        </ul>
+      </nav>
     </header>
   );
 };
