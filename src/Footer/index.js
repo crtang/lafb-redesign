e95a909938494c449069fe8f6105622eecd352e8
ctang <11547113+crtang@users.noreply.github.com>
2024-08-25 23:08:12 -0700

    Finished adding images and styling the site header and footer (for now) :

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,8 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import lafbFb from '../imgs/facebook.svg';
+import lafbFbHover from '../imgs/facebook-hover.svg';
+import lafbInsta from '../imgs/instagram.svg';
+import lafbInstaHover from '../imgs/instagram-hover.svg';
 
 const Footer = () => {
+  //const [fbLogo, setFbLogo] = useState(lafbFB);
+  //const [instaLogo, setInstaLogo] = useState(lafbInsta);
+
   return (
-    <footer></footer>
+    <footer>
+      <div id="hours">
+        <h2>Hours</h2>
+        <p>
+          Tuesday - Saturday | 10 am to 6 pm
+        </p>
+      </div>
+
+      <div id="location">
+        <h2>Location</h2>
+        <p>
+          Next to the historic first location of Peet's Coffee
+          <br />
+          Walnut Square
+          <br />
+          2122 Vine St.
+          <br />
+          Berkeley, CA
+        </p>
+      </div>
+
+      <div id="social-and-policies">
+        <p>
+          <a href="https://www.facebook.com/loveatfirstbiteberkeley/" target="_blank">
+            <img
+              className="social-media"
+              src={lafbFb}
+              alt="Facebook page for Love At First Bite Bakery"
+              onMouseOver={e => (e.currentTarget.src = lafbFbHover )}
+              onMouseOut={ e => (e.currentTarget.src = lafbFb )}
+            />
+          </a>
+
+          <a href="https://www.instagram.com/love.at.first.bite/" target="_blank">
+            <img
+              className="social-media"
+              src={lafbInsta}
+              alt="Instagram for Love At First Bite Bakery"
+              onMouseOver={e => (e.currentTarget.src = lafbInstaHover )}
+              onMouseOut={ e => (e.currentTarget.src = lafbInsta )}
+            />
+          </a>
+        </p>
+
+        <p>
+          <a href="#" target="_blank">Policies</a>
+        </p>
+      </div>
+    </footer>
   );
 };
 
--- src/Header/index.js
+++ src/Header/index.js
@@ -8,12 +8,16 @@ const Header = () => {
         <img id="lafb-logo" src={lafbLogo} alt="Love At First Bite Bakery logo" />
       </div>
 
+      <p id="alert">
+        Please use the ORDER link to place an order for pick-up or delivery as soon as today.
+      </p>
+
       <nav>
         <ul>
-          <li>HOME</li>
-          <li>MENU</li>
-          <li>ORDER</li>
-          <li>PRESS</li>
+          <a href=""><li>HOME</li></a>
+          <a href=""><li>MENU</li></a>
+          <a href=""><li>ORDER</li></a>
+          <a href=""><li>PRESS</li></a>
         </ul>
       </nav>
     </header>
