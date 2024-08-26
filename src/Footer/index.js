d1182dd5b07b269d19bb1567d807a5263b736083
ctang <11547113+crtang@users.noreply.github.com>
2024-08-26 12:01:15 -0700

    Added disclaimer in footer and tweaked header and footer design for better responsiveness

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,60 +1,68 @@
-import React, { useState } from 'react';
+import React from 'react';
 import lafbFb from '../imgs/facebook.svg';
 import lafbFbHover from '../imgs/facebook-hover.svg';
 import lafbInsta from '../imgs/instagram.svg';
 import lafbInstaHover from '../imgs/instagram-hover.svg';
 
 const Footer = () => {
-  //const [fbLogo, setFbLogo] = useState(lafbFB);
-  //const [instaLogo, setInstaLogo] = useState(lafbInsta);
-
   return (
     <footer>
-      <div id="hours">
-        <h2>Hours</h2>
-        <p>
-          Tuesday - Saturday | 10 am to 6 pm
-        </p>
+      <div id="store-info">
+        <div id="hours">
+          <h2>Hours</h2>
+          <p>
+            Tuesday - Saturday | 10 am to 6 pm
+          </p>
+        </div>
+
+        <div id="location">
+          <h2>Location</h2>
+          <p>
+            Next to the historic first location of Peet's Coffee
+            <br />
+            Walnut Square
+            <br />
+            2122 Vine St.
+            <br />
+            Berkeley, CA
+          </p>
+        </div>
+
+        <div id="social-and-policies">
+          <p>
+            <a href="https://www.facebook.com/loveatfirstbiteberkeley/" target="_blank">
+              <img
+                className="social-media"
+                src={lafbFb}
+                alt="Facebook page for Love At First Bite Bakery"
+                onMouseOver={e => (e.currentTarget.src = lafbFbHover )}
+                onMouseOut={ e => (e.currentTarget.src = lafbFb )}
+              />
+            </a>
+
+            <a href="https://www.instagram.com/love.at.first.bite/" target="_blank">
+              <img
+                className="social-media"
+                src={lafbInsta}
+                alt="Instagram for Love At First Bite Bakery"
+                onMouseOver={e => (e.currentTarget.src = lafbInstaHover )}
+                onMouseOut={ e => (e.currentTarget.src = lafbInsta )}
+              />
+            </a>
+          </p>
+
+          <p>
+            <a href="#" target="_blank">Policies</a>
+          </p>
+        </div>
       </div>
 
-      <div id="location">
-        <h2>Location</h2>
+      <div id="copyright-and-disclaimer">
         <p>
-          Next to the historic first location of Peet's Coffee
-          <br />
-          Walnut Square
-          <br />
-          2122 Vine St.
-          <br />
-          Berkeley, CA
-        </p>
-      </div>
-
-      <div id="social-and-policies">
-        <p>
-          <a href="https://www.facebook.com/loveatfirstbiteberkeley/" target="_blank">
-            <img
-              className="social-media"
-              src={lafbFb}
-              alt="Facebook page for Love At First Bite Bakery"
-              onMouseOver={e => (e.currentTarget.src = lafbFbHover )}
-              onMouseOut={ e => (e.currentTarget.src = lafbFb )}
-            />
-          </a>
-
-          <a href="https://www.instagram.com/love.at.first.bite/" target="_blank">
-            <img
-              className="social-media"
-              src={lafbInsta}
-              alt="Instagram for Love At First Bite Bakery"
-              onMouseOver={e => (e.currentTarget.src = lafbInstaHover )}
-              onMouseOut={ e => (e.currentTarget.src = lafbInsta )}
-            />
-          </a>
-        </p>
-
-        <p>
-          <a href="#" target="_blank">Policies</a>
+          All information and photos belong to Love At First Bite Bakery.
+          This site is simply a redesign I created, which I suppose is a bit
+          similar to the redesign case studies of UI/UX designers &#40;without
+          the formal training or user research parts&#41;.
         </p>
       </div>
     </footer>
